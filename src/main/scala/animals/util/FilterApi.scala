04da74e22e7576e2e2c4f1fdbe6240e0adff5635
Daniil Tikhomirov <you@example.com>
2020-10-31 22:50:59 +0300

    s

--- src/main/scala/animals/util/FilterApi.scala
+++ src/main/scala/animals/util/FilterApi.scala
@@ -1,11 +1,18 @@
 package animals.util
 
-import org.squeryl.dsl.TypedExpression
+import org.squeryl.dsl.{TOptionLong, TypedExpression}
 import xitrum.scope.request.Params
+import RichParams._
 
 object FilterApi {
 
 //  implicit class FilterSyntax(params: Params) {
-//    def numeric(field: TypedExpression)
+//    def numeric[T, U <: TOptionLong](field: TypedExpression[U, T], filterName: String) = {
+//      val op = params.byName(s"${filterName}Op").getOrElse("eq")
+//
+//      op match {
+//        case "eq" => field === params.byName[Long](filterName)
+//      }
+//    }
 //  }
 }
